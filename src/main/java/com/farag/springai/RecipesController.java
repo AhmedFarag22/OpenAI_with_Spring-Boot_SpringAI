c54728b356d094424de7633a5b6e08b62304eb8c
Ahmed Farag <farag0336@gmail.com>
2025-10-04 22:20:08 +0300

    Done

---
+++ src/main/java/com/farag/springai/RecipesController.java
@@ -0,0 +1,34 @@
+package com.farag.springai;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recipes")
+public class RecipesController {
+
+    private final ChatClient chatClient;
+
+    public RecipesController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/suggest-recipe")
+    public String suggestRecipe(
+            @RequestParam(
+                    name = "message",
+                    defaultValue = "Suggest a recipe for dinner"
+            ) String message
+    ) {
+        return this.chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+}
+
+
+
